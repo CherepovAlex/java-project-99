1b8cd646b79cf63f4cb07ac4154a7d9a8dfbb868
CherepovAlex <westler123@yandex.ru>
2025-07-27 11:45:06 +0300

    first commit setup

--- src/main/java/hexlet/code/controller/WelcomeController.java
+++ src/main/java/hexlet/code/controller/WelcomeController.java
@@ -4,9 +4,9 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-public class WelcomeController {
+public final class WelcomeController {
 
-    @GetMapping
+    @GetMapping("/welcome")
     public String welcome() {
         return "Welcome to Spring!";
     }
